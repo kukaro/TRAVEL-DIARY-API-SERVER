ca92a765f29ab4d1b133d182f8aedbdcdb92ef59
kukaro <justkukaro@naver.com>
2020-06-15 11:37:25 +0900

    add health API

--- project/src/project/routes/api.php
+++ project/src/project/routes/api.php
@@ -12,9 +12,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::get('/user', function (Request $request) {
-    dump($request);
     return $request->user();
+});
+
+Route::get('/health', function (Request $request) {
+    return ['MSG' => 'OK', 'STATUS' => 200];
 });
