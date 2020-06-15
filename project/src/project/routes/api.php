c8306bce4f8c350fd84789687fbb8cd6db08eac5
kukaro <justkukaro@naver.com>
2020-06-15 11:08:10 +0900

    인증 제거

--- project/src/project/routes/api.php
+++ project/src/project/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
+    dump($request);
     return $request->user();
 });
