95f72931a50198ebd76c0642909cfe11e753342b
kukaro <justkukaro@naver.com>
2020-06-25 11:11:41 +0900

    중간 커밋지점 - Join로직 오류나면 여기로 롤백합니다.

--- project/src/project/app/Http/Dto/DtoImpl.php
+++ project/src/project/app/Http/Dto/DtoImpl.php
@@ -4,4 +4,9 @@ namespace App\Http\Dto;
 
 abstract class DtoImpl implements Dto
 {
+    private $joins = [];
+    public function addJoin(Join $join)
+    {
+        $joins[$join->target_table_name] = $join;
+    }
 }
