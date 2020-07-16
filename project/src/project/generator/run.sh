7601bf69811d4aa54ddb86dc272eb4289f11cfcd
kukaro <justkukaro@naver.com>
2020-07-16 09:54:48 +0900

    generator로직에서 autoload, linux에서 대입되게 수정

--- project/src/project/generator/run.sh
+++ project/src/project/generator/run.sh
@@ -1,6 +1,6 @@
-RELATIVE_DIR=`dirname "$0"`
-cd $RELATIVE_DIR
-SHELL_PATH=`pwd -P`
-cd $SHELL_PATH
-php generator.php deafult api $1
-php ../artisan make:model $1
+RELATIVE_DIR=$(dirname "$0")
+cd "$RELATIVE_DIR"
+SHELL_PATH=$(pwd -P)
+cd "$SHELL_PATH"
+php generator.php deafult api "$1"
+php ../artisan make:model "$1"
