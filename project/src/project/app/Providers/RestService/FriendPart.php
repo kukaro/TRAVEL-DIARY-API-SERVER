ab5234196af8a86eb43da0c8995dfffa37232b9b
kukaro <justkukaro@naver.com>
2020-07-30 09:54:31 +0900

    friend api 뼈대들을 추가

---
+++ project/src/project/app/Providers/RestService/FriendPart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers\RestService;
+
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\FriendController;
+use App\Http\Repositories\FriendRepository;
+use App\Http\Requests\RestRequests\RestRequest;
+use App\Http\Requests\RestRequests\FriendRestRequest;
+use App\Http\Services\Classes\FriendServiceImpl;
+use App\Http\Services\Interfaces\FriendService;
+use Illuminate\Support\ServiceProvider;
+
+class FriendPart{
+    static function run()
+    {
+        app()->singleton(FriendRestRequest::class, function () {
+            return new FriendRestRequest();
+        });
+
+        app()->singleton(FriendService::class, function () {
+            return new FriendServiceImpl(new FriendRepository());
+        });
+
+        app()->singleton(FriendController::class, function () {
+            return new FriendController(app()->make(FriendService::class), app()->make(FriendRestRequest::class));
+        });
+
+    }
+
+    static function mainRun(){
+        app()->singleton(RestRequest::class, function () {
+            return app()->make(FriendRestRequest::class);
+        });
+    }
+}
