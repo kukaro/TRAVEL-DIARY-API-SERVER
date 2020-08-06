0bfa0004e8bf4ee827b4803bc47aa96322faba63
kukaro <justkukaro@naver.com>
2020-08-06 09:40:23 +0900

    에러 타입 추가

--- project/src/project/app/Util/DB/ErrorTypeBuilder.php
+++ project/src/project/app/Util/DB/ErrorTypeBuilder.php
@@ -8,16 +8,24 @@ class ErrorTypeBuilder
 {
     static public function get(QueryException $e)
     {
-        if($e->getPrevious()->errorInfo==null){
-            return ErrorType::$STOP;
+        dump($e->getCode());
+        if (is_null($e->getPrevious()->errorInfo)) {
+            switch ($e->getCode()){
+                case 1002:
+                    return ErrorType::$REFUSE;
+                case 1045:
+                    return ErrorType::$LOGIN_FAIL;
+            }
         }
         $error = $e->getPrevious()->errorInfo[1];
         $sql_state = $e->getPrevious()->errorInfo[0];
         switch ($error . ':' . $sql_state) {
             case '1062:23000':
                 return ErrorType::$DUP;
+            case '1048:23000':
+                return ErrorType::$LACK;
             default:
-                return ErrorType::$UND;
+                return ErrorType::getUnd($e->getPrevious()->errorInfo[2]);
         }
     }
 }
