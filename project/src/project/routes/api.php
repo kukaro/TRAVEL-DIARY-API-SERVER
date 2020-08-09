12d66ea16bed0a6d48ac977791840241b53f8beb
kukaro <justkukaro@naver.com>
2020-08-10 08:57:03 +0900

    code indent

--- project/src/project/routes/api.php
+++ project/src/project/routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', 'JWTAuthController@user')->name('api
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user', 'JWTAuthController@user')->name('api.jwt.user');
 });
-Route::middleware('auth:api', 'can:general,'.User::class)
+Route::middleware('auth:api', 'can:general,' . User::class)
     ->get('/user/friend', 'UserController@getLinkedFriend')->name('user\getLinkedFriend');
 Route::middleware('auth:api')
     ->get('/user/post-comment/post/{id}', 'UserController@getByPostComment')->name('user\getByPostComment');
@@ -31,7 +31,7 @@ Route::post('/user', 'UserController@post')->name('user\post');
 Route::patch('/user/{email}', 'UserController@patch')->name('user\patch');
 Route::delete('/user/{email}', 'UserController@delete')->name('user\delete');
 
-Route::middleware('auth:api', 'can:general,'.User::class)
+Route::middleware('auth:api', 'can:general,' . User::class)
     ->get('/picture/user/{id}', 'PictureController@getWithUser')->name('picture\getWithUser');
 Route::get('/picture/{id}', 'PictureController@get')->name('picture\get');
 Route::post('/picture', 'PictureController@post')->name('picture\post');
@@ -41,14 +41,14 @@ Route::delete('/picture/{id}', 'PictureController@delete')->name('picture\delete
 Route::get('/post-picture/{id}', 'PostPictureController@get')->name('post\get');
 Route::post('/post-picture', 'PostPictureController@post')->name('post\post');
 
-Route::middleware('auth:api', 'can:general,'.User::class)
+Route::middleware('auth:api', 'can:general,' . User::class)
     ->get('/post-comment/post/{id}', 'PostCommentController@getWithPost')->name('postcomment\getWithPost');
-Route::middleware('auth:api', 'can:general,'.User::class)
+Route::middleware('auth:api', 'can:general,' . User::class)
     ->get('/post-comment/{id}', 'PostCommentController@get')->name('postcomment\get');
 Route::post('/post-comment', 'PostCommentController@post')->name('postcomment\post');
 
 
-Route::middleware('auth:api', 'can:general,'.User::class)
+Route::middleware('auth:api', 'can:general,' . User::class)
     ->get('/post/user', 'PostController@getWithUser')->name('post\getWithUser');
 Route::middleware('auth:api')->get('/post/{id}', 'PostController@get')->name('post\get');
 Route::middleware('auth:api')->post('/post', 'PostController@post')->name('post\post');
@@ -56,10 +56,10 @@ Route::middleware('auth:api')->patch('/post/{id}', 'PostController@patch')->name
 Route::middleware('auth:api')->delete('/post/{id}', 'PostController@delete')->name('post\delete');
 Route::get('/post/picture/{id}', 'PostController@getWithPicture')->name('post\getWithPicture');
 
-Route::middleware('auth:api', 'can:general,'.User::class)
-    ->get('/friend','FriendController@get')->name('friend\get');
-Route::middleware('auth:api', 'can:general,'.User::class)
-    ->post('/friend','FriendController@post')->name('friend\post');
+Route::middleware('auth:api', 'can:general,' . User::class)
+    ->get('/friend', 'FriendController@get')->name('friend\get');
+Route::middleware('auth:api', 'can:general,' . User::class)
+    ->post('/friend', 'FriendController@post')->name('friend\post');
 
 Route::get('/health', function (Request $request) {
     return ['MSG' => 'OK', 'STATUS' => 200];
@@ -75,6 +75,6 @@ Route::get('/file/{catchall}', 'FileController@get')->where('catchall', '.*')->n
 Route::post('/file', 'FileController@post')->name('file\post');
 Route::delete('/file/{catchall}', 'FileController@delete')->where('catchall', '.*')->name('file\delete');
 
-Route::get('/hiworks/callback','HiworksController@callback')->name('hiworks\callback');
-Route::get('/hiworks','HiworksController@get')->name('hiworks\get');
+Route::get('/hiworks/callback', 'HiworksController@callback')->name('hiworks\callback');
+Route::get('/hiworks', 'HiworksController@get')->name('hiworks\get');
 
